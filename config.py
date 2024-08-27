20b981b24f71c28ad1a9c5efff2c15a3fe0553a7
ashtoff <alex.shtoff@yahooinc.com>
2024-08-27 09:10:23 +0300

    arcsinh-squared-normalization, 2/7 trainset.

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ transform_config = {
 }
 
 int_cols = [f'I_{1 + i}' for i in range(13)]
-spline_cols = ['I_2', 'I_5', 'I_6', 'I_9']
+spline_cols = ['I_1', 'I_2', 'I_3', 'I_4', 'I_5', 'I_6', 'I_7', 'I_8', 'I_9', 'I_11', 'I_13']
 cat_cols = [f'C_{1 + i}' for i in range(26)]
 columns = ['label'] + int_cols + cat_cols
 degrees = [0, 3]
 
-split_fractions = [1./7, 1./7, 1./7]
+split_fractions = [2./7, 1./7, 1./7]
 out_prefix = '/home/ashtoff/ephemeral_drive/criteo_preprocess'
 out_train_path = f'{out_prefix}tr'
 out_val_path = f'{out_prefix}val'
